7b181f5f511c52f659744db3732efc836bd3fbd1
n5frzw92y4-blip <n5frzw92y4@privaterelay.appleid.com>
2026-07-14 14:44:29 +0100

    add tabs to Product infolist and enhance PostsTable features

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -5,10 +5,14 @@ namespace App\Filament\Resources\Posts\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class PostsTable
@@ -17,14 +21,48 @@ class PostsTable
     {
         return $table
             ->columns([
-                ImageColumn::make('image')->disk("public"),
-                TextColumn::make('title'),
-                TextColumn::make('slug'),
-                TextColumn::make('category.name'),
+                TextColumn::make("id")
+                    ->label("ID")
+                    ->toggleable(isToggledHiddenByDefault: true),
+                ImageColumn::make('image')->disk("public")->toggleable(),
+                TextColumn::make('title')
+                    ->sortable()
+                    ->searchable()->toggleable(),
+                TextColumn::make('slug')
+                    ->sortable()
+                    ->searchable()->toggleable(),
+                TextColumn::make('category.name')
+                    ->sortable()
+                    ->searchable()->toggleable(),
                 ColorColumn::make('color'),
-            ])
+                TextColumn::make('created_at')
+                    ->label("created At ")
+                    ->dateTime()
+                    ->sortable()->toggleable(),
+                TextColumn::make("tags")
+                    ->label("tags")
+                    ->toggleable(isToggledHiddenByDefault: true),
+                IconColumn::make("published")
+                    ->boolean()
+                    ->toggleable(isToggledHiddenByDefault: true)
+            ])->defaultSort("title","asc")
             ->filters([
-                //
+                Filter::make("created_at")
+                    ->label("Creation Date:")
+                    ->schema([
+                        DatePicker::make("created_at")
+                            ->label("Select Date:")
+                    ])
+                    ->query(function($query, $data){
+                        return $query
+                            ->when($data["created_at"],function($q, $date){
+                                $q->whereDate("created_at",$date);
+                            });
+                    }),
+                SelectFilter::make("category_id")
+                    ->label("Select Category")
+                    ->relationship("category", "name")
+                    ->preload()
             ])
             ->recordActions([
                 EditAction::make(),
--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -6,6 +6,8 @@ use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Support\Icons\Heroicon;
 
 class ProductInfolist
@@ -14,60 +16,79 @@ class ProductInfolist
     {
         return $schema
             ->components([
-                Section::make("Product Info")
-                    ->schema([
-                        TextEntry::make("id")
-                            ->label("Product ID")
-                            ->weight("bold")
-                            ->color('primary'),
-                        TextEntry::make("name")
-                            ->label("Product Name")
-                            ->weight("bold")
-                            ->color('primary'),
-                        TextEntry::make("sku")
-                            ->label("Product SKU")
-                            ->weight("bold")
-                            ->badge()
-                            ->color('success'),
-                        TextEntry::make("description")
-                            ->label("Product Description")
-                            ->weight("bold")
-                            ->color('primary'),
-                        TextEntry::make("created_at")
-                            ->label("Product Creation Date")
-                            ->weight("bold")
-                            ->color('primary')
-                            ->date("m/d/Y"),
-                    ])->columnSpanFull(),
-
-
-                Section::make("Pricing & Stock")
-                    ->schema([
-                        TextEntry::make("price")
-                            ->label("Product Price")
-                            ->weight("bold")
+                Tabs::make("Tabs")
+                    ->tabs([
+                        Tab::make("Product Info")
+                            ->icon(Heroicon::AcademicCap)
+                            ->schema([
+                                TextEntry::make("id")
+                                    ->label("Product ID")
+                                    ->weight("bold")
+                                    ->color('primary'),
+                                TextEntry::make("name")
+                                    ->label("Product Name")
+                                    ->weight("bold")
+                                    ->color('primary'),
+                                TextEntry::make("sku")
+                                    ->label("Product SKU")
+                                    ->weight("bold")
+                                    ->badge()
+                                    ->color('success'),
+                                TextEntry::make("description")
+                                    ->label("Product Description")
+                                    ->weight("bold")
+                                    ->color('primary'),
+                                TextEntry::make("created_at")
+                                    ->label("Product Creation Date")
+                                    ->weight("bold")
+                                    ->color('primary')
+                                    ->date("m/d/Y"),
+                            ]),
+                        Tab::make("Pricing & Stock")
                             ->icon(Heroicon::CurrencyDollar)
-                            ->color('primary'),
-                        TextEntry::make("stock")
-                            ->label("Product Stock")
-                            ->weight("bold")
-                            ->color('primary'),
+                            ->badge(10)
+                            ->badgeColor('info')
+                            ->schema([
+                                TextEntry::make("price")
+                                    ->label("Product Price")
+                                    ->weight("bold")
+                                    ->icon(Heroicon::CurrencyDollar)
+                                    ->color('primary'),
+                                TextEntry::make("stock")
+                                    ->label("Product Stock")
+                                    ->weight("bold")
+                                    ->color('primary'),
+                            ]),
+                        Tab::make("Media & Status")
+                            ->icon(Heroicon::Photo)
+                            ->schema([
+                                ImageEntry::make("image")
+                                    ->label("Product Image")
+                                    ->disk("public"),
+                                IconEntry::make("is_active")
+                                    ->label("Is Active?")
+                                    ->boolean(),
+                                IconEntry::make("is_featured")
+                                    ->label("Is Featured?")
+                                    ->boolean(),
+                            ])
 
-                    ])->columnSpanFull(),
+                    ])->columnSpanFull()->vertical(),
 
-                Section::make("Media & Status")
-                    ->schema([
-                        ImageEntry::make("image")
-                            ->label("Product Image")
-                            ->disk("public"),
-                        IconEntry::make("is_active")
-                            ->label("Is Active?")
-                            ->boolean(),
-                        IconEntry::make("is_featured")
-                            ->label("Is Featured?")
-                            ->boolean(),
-
-                    ])->columnSpanFull(),
             ]);
     }
 }
+
+
+ /*               Section::make("Product Info")
+                    ->schema([
+                    ])->columnSpanFull(),
+                Section::make("Pricing & Stock")
+                    ->schema([
+                    ])->columnSpanFull(),
+                Section::make("Media & Status")
+                    ->schema([
+                    ])->columnSpanFull(),
+            ]);
+    }
+}*/
